ffde6b927d988d69790e38bea4b4b187314fa41e
ivanbilotserkivskiy <ivanbilotserkivskiy@gmail.com>
2021-07-16 12:57:56 +0300

    l/k

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
+const colors = ['#FFFFFF','#2196F3','#4CAF50','#FF9800','#009688','#795548',];
 
-const startRef = document.querySelectorAll('button');
-function getRamdomInt(min, max) {
-    return Math.floor(Math.random()* (max-min+1))+min
+const buttonRef = document.querySelectorAll('button');
 
-}
-let key = getRamdomInt(0, colors.length-1);
-console.log(key)
-function random(colors) {
-    return colors [getRamdomInt(0, colors.length-1)];
-    
-}
-// function random(color){
-// const randomIntegerFromInterval = (min, max) => {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-//   };}
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
 const bodyRef = document.querySelector('body');
-function handler(randomIntegerFromInterval, colors) {
-//   bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(colors[0],colors[5])];
-
+const handler = () => {
+  const a = randomIntegerFromInterval(1, 6);
+  bodyRef.style.backgroundColor = colors[a-1];
 };
-// const bodyColor = bodyRef.style.backgroundColor = 'red';
-startRef[0].addEventListener('click', setInterval);
-const timerId = setInterval(handler, 1000);
-console.log(randomIntegerFromInterval(colors))
+
+
+
+buttonRef[0].addEventListener('click', () => {
+    const intervalId = setInterval(handler, 1000);
+    buttonRef[0].setAttribute('disabled', 'true')
+    if(buttonRef[0].hasAttribute('disabled')=== false){
+        clearInterval(intervalId);
+    }
+
+});
+buttonRef[1].addEventListener('click', () => {
+    buttonRef[0].removeAttribute('disabled')
+});
+
+
